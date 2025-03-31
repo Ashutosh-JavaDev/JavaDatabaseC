380d9e9254415b1ce4ec02518c47bc11d9dc115e
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-01 02:10:05 +0530

    CURD_Application

--- CURD_Application/src/Create.java
+++ CURD_Application/src/Create.java
@@ -26,25 +26,22 @@ public class Create {
                 if (str.equalsIgnoreCase("Stop")) {
                     return;
                 } else {
-                    try {
-                        System.out.print("First Name: \n");
-                        FirstName = sc.nextLine();
-                        System.out.print("Last Name: \n");
-                        LastName = sc.nextLine();
-                        System.out.print("Email: \n");
-                        Email = sc.nextLine();
-                        System.out.print("Phone: \n");
-                        Phone = sc.nextInt();
-                        System.out.print("Course \n");
-                        Course = sc.nextLine();
-                        System.out.print("Batch \n");
-                        Batch = sc.nextInt();
-                        System.out.print("Gender \n");
-                        Gender = sc.nextLine();
-                    } catch (Exception e) {
-                        e.getMessage();
 
-                    }
+                    System.out.print("First Name: \n");
+                    FirstName = sc.nextLine();
+                    System.out.print("Last Name: \n");
+                    LastName = sc.nextLine();
+                    System.out.print("Email: \n");
+                    Email = sc.nextLine();
+                    System.out.print("Phone: \n");
+                    Phone = sc.nextInt();
+                    System.out.print("Course \n");
+                    Course = sc.nextLine();
+                    System.out.print("Batch \n");
+                    Batch = sc.nextInt();
+                    System.out.print("Gender \n");
+                    Gender = sc.nextLine();
+
                 }
 
                 PreparedStatement ps = con.prepareStatement("insert into Registration values(?,?,?,?,?,?,?)");
