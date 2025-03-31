56e884663c9cf56f5d76ebc128ca7ab95f89c272
Ashutosh Ayush <jbnashu297@gmail.com>
2025-03-31 22:02:38 +0530

    CURD_Application

--- CURD_Application/src/Create.java
+++ CURD_Application/src/Create.java
@@ -3,6 +3,6 @@ import java.sql.Connection;
 public class Create {
     public static void main(String[] args) throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con=DriverManager.getConnection("null", "root", "@Radhakrishna297");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Management", "root", "@Radhakrishna297");
     }
 }
