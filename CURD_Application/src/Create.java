23d295c7ea67a7a17e0d6c0d632792d7e9946343
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-01 00:48:53 +0530

    CURD_Application

--- CURD_Application/src/Create.java
+++ CURD_Application/src/Create.java
@@ -17,6 +17,21 @@ public class Create {
         int Batch;
         String Gender = "";
         while (true) {
+            System.out.print("First Name: \n");
+            FirstName=sc.nextLine();
+            System.out.print("Last Name: \n");
+            LastName=sc.nextLine();
+            System.out.print("Email: \n");
+            Email=sc.nextLine();
+            System.out.print("Phone: \n");
+            Phone=sc.nextInt();
+            System.out.print("Course \n");
+            Course=sc.nextLine();
+            System.out.print("Batch \n");
+            Batch=sc.nextInt();
+            System.out.print("Gender \n");
+            Gender=sc.nextLine();
+
             PreparedStatement ps = con.prepareStatement("insert into Registration values(?,?,?,?,?,?,?,?)");
             int count = ps.executeUpdate();
             if (count > 0) {
