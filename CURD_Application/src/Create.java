cf7c60482d1d18edd2be431b351fe19fb134d639
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-01 00:41:23 +0530

    CURD_Application

--- CURD_Application/src/Create.java
+++ CURD_Application/src/Create.java
@@ -1,17 +1,22 @@
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.util.Scanner;
+
 public class Create {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner("System.in");
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Management", "root", "@Radhakrishna297");
-        PreparedStatement ps=con.prepareStatement("insert into Registration values(?,?,?,?,?,?,?,?)");
-        int count=ps.executeUpdate();
-        if(count>0){
-            System.out.println("Data inserted Successfully");
-        }
-        else{
-            System.out.println("Data Not Been Inserted");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Management", "root",
+                "@Radhakrishna297");
+        while (true) {
+            PreparedStatement ps = con.prepareStatement("insert into Registration values(?,?,?,?,?,?,?,?)");
+            int count = ps.executeUpdate();
+            if (count > 0) {
+                System.out.println("Data inserted Successfully");
+            } else {
+                System.out.println("Data Not Been Inserted");
+            }
         }
     }
 }
