66b47e723b6b394db0234095fa23513545522def
Ashutosh Ayush <jbnashu297@gmail.com>
2025-03-31 18:37:43 +0530

    CURD

--- CURD_Application/src/Create.java
+++ CURD_Application/src/Create.java
@@ -1,5 +1,6 @@
 public class Create {
     public static void main(String[] args) throws Exception {
-        Class.forName("");
+        Class.forName("mysql.cj.jdbc.Driver");
+        System.out.println("success");
     }
 }
