b5afcb03ca3e7ba328ecf7d1eb48036e45f0e2e2
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-01 16:24:34 +0530

    CURD_Application

--- CURD_Application/src/Update.java
+++ CURD_Application/src/Update.java
@@ -1,7 +1,21 @@
 import java.sql.Statement;
+import java.util.InputMismatchException;
 import java.sql.PreparedStatement;
 import java.sql.DriverManager;
 
-public class Update {
+class updation {
+    public static void update() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
 
+        }
+    }
+}
+
+public class Update {
+    public static void main(String[] args) {
+
+    }
 }
