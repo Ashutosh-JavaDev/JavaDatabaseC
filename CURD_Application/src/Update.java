70fd6be4a9290c768d304939f4493a514437a7ea
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-01 22:11:40 +0530

    CURD

--- CURD_Application/src/Update.java
+++ CURD_Application/src/Update.java
@@ -12,6 +12,8 @@ class updation {
             Connection con=DriverManager.getConnection("jbdc:mysql://localhost:3306/Libary_Mangement", "root", "@Radhakrishna297");
             System.out.println("Enter the Student ID whose Data want to Update");
             int Student=sc.nextInt();
+            System.out.println("Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
+            int choice=sc.nextInt();
             PreparedStatement ps=con.prepareStatement("Update Registration set ");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
