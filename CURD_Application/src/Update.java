5bdcac0572fabdc573b43955520d2f20e668f18e
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-01 20:39:47 +0530

    CURD

--- CURD_Application/src/Update.java
+++ CURD_Application/src/Update.java
@@ -1,13 +1,21 @@
 import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.DriverManager;
-
+import java.sql.Connection;
+import java.util.Scanner;
 class updation {
     public static void update() {
+        Scanner sc=new Scanner(System.in);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection("jbdc:mysql://localhost:3306/Libary_Mangement", "root", "@Radhakrishna297");
+            System.out.println("");
+            PreparedStatement ps=con.prepareStatement("Update Registration set ");
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(SQLException e){
             e.printStackTrace();
         }
     }
