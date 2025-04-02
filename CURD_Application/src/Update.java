d6cf44e795e64c00673304052e09df5263b812d3
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-02 23:02:53 +0530

    CURD

--- CURD_Application/src/Update.java
+++ CURD_Application/src/Update.java
@@ -11,12 +11,12 @@ class updation {
         Scanner sc = new Scanner(System.in);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jbdc:mysql://localhost:3306/Libary_Mangement", "root",
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement", "root",
                     "@Radhakrishna297");
             System.out.println("Enter the Student ID whose Data want to Update");
             int Student = sc.nextInt();
             Statement smt = con.createStatement();
-            ResultSet re = smt.executeQuery("Select*from Registration");
+            ResultSet re = smt.executeQuery("Select '"+Student+"'from Registration");
             int count = 0;
             while (re.next()) {
                 count++;
