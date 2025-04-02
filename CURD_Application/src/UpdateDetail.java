bc673395830cf3b81d914e01b1de3014344c711a
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 00:43:40 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -5,7 +5,15 @@ import java.sql.DriverManager;
 import java.util.Scanner;
 class updateDetail{
     public void Updatedetail(){
-        
+        Scanner sc=new Scanner(System.in);
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:/3306/Libary_Mangement", "root", "@Radhakrishna297");
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
 public class UpdateDetail {
