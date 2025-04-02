85efc7ba94855698e259693785e7f6ae7918e952
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-02 22:00:55 +0530

    CURD

--- CURD_Application/src/Update.java
+++ CURD_Application/src/Update.java
@@ -1,5 +1,6 @@
 import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.Connection;
@@ -12,11 +13,14 @@ class updation {
             Connection con=DriverManager.getConnection("jbdc:mysql://localhost:3306/Libary_Mangement", "root", "@Radhakrishna297");
             System.out.println("Enter the Student ID whose Data want to Update");
             int Student=sc.nextInt();
+            Statement smt=con.createStatement();
+            ResultSet re=smt.executeQuery("Select*from Registration");
             System.out.println("Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
             int choice=sc.nextInt();
             switch(choice){
-                
-            }
+               case 1:
+               
+            }  
             PreparedStatement ps=con.prepareStatement("Update Registration set ");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
