e932915f9330dddb6a1fb940e8679d34189ac0ea
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 00:23:29 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -1,3 +1,7 @@
-public class UpdateDetail {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.util.Scanner;public class UpdateDetail {
     
 }
