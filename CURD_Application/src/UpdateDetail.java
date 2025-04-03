7997e73a1fa26ffb06117903f4666328997db6aa
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 11:27:56 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.util.Scanner;
@@ -11,7 +12,15 @@ class updateDetail {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement", "root",
                     "@Radhakrishna297");
-            System.out.println("Code is Running");
+            System.out.println("Enter the Student ID whose data want to Update");
+            int student=sc.nextInt();
+            Statement smt=con.createStatement();
+            ResultSet rs=smt.executeQuery("Select STU_ID from Registration");
+            if(rs.next()){
+                System.out.println(
+                    "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
+                    int choice=sc.nextInt();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
