e42686580feafdd013e3790430d1093dcf8b9e92
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 12:03:44 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -20,19 +20,35 @@ class updateDetail {
             ResultSet rs=ps.executeQuery();
             int count=0;
             if(rs.next()){
-                System.out.println(
-                    "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
-                    int choice=sc.nextInt();
-                    switch(choice){
-                        case 1:
-
-                    }
+               rs.getInt(count);
             }
-            else{
+            if(count==0){
                 System.out.println("No Student is Found with this student ID in the Table");
             }
+            System.out.println(
+                "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
+                int choice=sc.nextInt();
+                sc.nextLine();
+                String ColumntoUpdate=" ";
+                switch(choice){
+                    case 1:
 
-        } catch (Exception e) {
+                }
+            System.out.println("Enter the value want to Update in :"+ColumntoUpdate);
+            String newUpdate=sc.nextLine();
+            String UpdateQuery=("Update Registration set "+ColumntoUpdate+"=? where STU_ID = ?");
+            PreparedStatement pst=con.prepareStatement(UpdateQuery);
+            pst.setString(1, newUpdate);
+            pst.setInt(2, student);
+            int value=pst.executeUpdate();
+            if(value>0){
+                System.out.println("Data Updated Successfully");
+            }
+            else{
+                System.out.println("Data has Not Been Updated");
+            }
+
+            } catch (Exception e) {
             e.printStackTrace();
         }
     }
