4ed49a32b9e4f3bd0a6c729881d764275d867557
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 12:05:26 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -13,42 +13,61 @@ class updateDetail {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement", "root",
                     "@Radhakrishna297");
             System.out.println("Enter the Student ID whose data want to Update");
-            int student=sc.nextInt();
-            String query=("Select STU_ID = ? from Registration");
-            PreparedStatement ps=con.prepareStatement(query);
-            ps.setInt(1,student);
-            ResultSet rs=ps.executeQuery();
-            int count=0;
-            if(rs.next()){
-               rs.getInt(count);
+            int student = sc.nextInt();
+            String query = ("Select STU_ID = ? from Registration");
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, student);
+            ResultSet rs = ps.executeQuery();
+            int count = 0;
+            if (rs.next()) {
+                rs.getInt(count);
             }
-            if(count==0){
+            if (count == 0) {
                 System.out.println("No Student is Found with this student ID in the Table");
             }
             System.out.println(
-                "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
-                int choice=sc.nextInt();
-                sc.nextLine();
-                String ColumntoUpdate=" ";
-                switch(choice){
-                    case 1:
+                    "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            String ColumntoUpdate = " ";
+            switch (choice) {
+                case 1:
+                    ColumntoUpdate = "FirstName";
+                    break;
+                case 1:
+                    ColumntoUpdate = "FirstName";
+                    break;
+                case 1:
+                    ColumntoUpdate = "FirstName";
+                    break;
+                case 1:
+                    ColumntoUpdate = "FirstName";
+                    break;
+                case 1:
+                    ColumntoUpdate = "FirstName";
+                    break;
+                case 1:
+                    ColumntoUpdate = "FirstName";
+                    break;
+                case 1:
+                    ColumntoUpdate = "FirstName";
+                    break;
 
-                }
-            System.out.println("Enter the value want to Update in :"+ColumntoUpdate);
-            String newUpdate=sc.nextLine();
-            String UpdateQuery=("Update Registration set "+ColumntoUpdate+"=? where STU_ID = ?");
-            PreparedStatement pst=con.prepareStatement(UpdateQuery);
+            }
+            System.out.println("Enter the value want to Update in :" + ColumntoUpdate);
+            String newUpdate = sc.nextLine();
+            String UpdateQuery = ("Update Registration set " + ColumntoUpdate + "=? where STU_ID = ?");
+            PreparedStatement pst = con.prepareStatement(UpdateQuery);
             pst.setString(1, newUpdate);
             pst.setInt(2, student);
-            int value=pst.executeUpdate();
-            if(value>0){
+            int value = pst.executeUpdate();
+            if (value > 0) {
                 System.out.println("Data Updated Successfully");
-            }
-            else{
+            } else {
                 System.out.println("Data has Not Been Updated");
             }
 
-            } catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
