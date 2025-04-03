78079124c9401f02dda549552c772e747eefe337
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 11:12:27 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -3,22 +3,24 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.util.Scanner;
-class updateDetail{
-    public void Updatedetail(){
-        Scanner sc=new Scanner(System.in);
-        try{
+
+class updateDetail {
+    public void Updatedetail() {
+        Scanner sc = new Scanner(System.in);
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement", "root", "@Radhakrishna297");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement", "root",
+                    "@Radhakrishna297");
             System.out.println("Code is Running");
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
 public class UpdateDetail {
     public static void main(String[] args) {
-        updateDetail ob=new updateDetail();
+        updateDetail ob = new updateDetail();
         ob.Updatedetail();
     }
 }
