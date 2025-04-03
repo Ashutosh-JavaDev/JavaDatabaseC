574ce16754ccb5350b834f0fcb8b734cfcc87124
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 23:37:51 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 class updateDetail {
     public void Updatedetail() {
+        // String ColumntoUpdate = " ";
         Scanner sc = new Scanner(System.in);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -14,7 +15,7 @@ class updateDetail {
                     "@Radhakrishna297");
             System.out.println("Enter the Student ID whose data want to Update");
             int student = sc.nextInt();
-            String query = ("Select STU_ID = ? from Registration");
+            String query = "SELECT COUNT(*) FROM Registration WHERE STU_ID = ?";
             PreparedStatement ps = con.prepareStatement(query);
             ps.setInt(1, student);
             ResultSet rs = ps.executeQuery();
@@ -26,41 +27,42 @@ class updateDetail {
                 System.out.println("No Student is Found with this student ID in the Table");
             }
             System.out.println(
-                "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
-        int choice = sc.nextInt();
-        sc.nextLine();
-        String ColumntoUpdate = " ";
-          
-           
-        switch (choice) {
-            case 1:
-                ColumntoUpdate = "FirstName";
-                break;
-            case 2:
-                ColumntoUpdate = "FirstName";
-                break;
-            case 3:
-                ColumntoUpdate = "FirstName";
-                break;
-            case 4:
-                ColumntoUpdate = "FirstName";
-                break;
-            case 5:
-                ColumntoUpdate = "FirstName";
-                break;
-            case 6:
-                ColumntoUpdate = "FirstName";
-                break;
-            case 7:
-                ColumntoUpdate = "FirstName";
-                break;
-                default:
-                System.out.println("Wrong press Choose wisely");
-                break;
+                    "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            String ColumntoUpdate = " ";
 
-        }
-           
-            System.out.println("Enter the value want to Update in :" + ColumntoUpdate);
+            // while (true) {
+                switch (choice) {
+                    case 1:
+                        ColumntoUpdate = "FirstName";
+                        break;
+                    case 2:
+                        ColumntoUpdate = "FirstName";
+                        break;
+                    case 3:
+                        ColumntoUpdate = "FirstName";
+                        break;
+                    case 4:
+                        ColumntoUpdate = "FirstName";
+                        break;
+                    case 5:
+                        ColumntoUpdate = "FirstName";
+                        break;
+                    case 6:
+                        ColumntoUpdate = "FirstName";
+                        break;
+                    case 7:
+                        ColumntoUpdate = "FirstName";
+                        break;
+                    default:
+                        System.out.println("Wrong press Choose wisely");
+                        break;
+
+                }
+            // }
+            System.out.println("Enter the value want to Update in :" +ColumntoUpdate);
+
             String newUpdate = sc.nextLine();
             String UpdateQuery = ("Update Registration set " + ColumntoUpdate + "=? where STU_ID = ?");
             PreparedStatement pst = con.prepareStatement(UpdateQuery);
