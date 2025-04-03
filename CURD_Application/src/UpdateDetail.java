dc5eb3b4519400bdd5c7e3dc805d17037775a4b4
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 11:49:26 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -14,11 +14,11 @@ class updateDetail {
                     "@Radhakrishna297");
             System.out.println("Enter the Student ID whose data want to Update");
             int student=sc.nextInt();
-            Statement smt=con.createStatement();
             String query=("Select STU_ID = ? from Registration");
             PreparedStatement ps=con.prepareStatement(query);
+            ps.setInt(1,student);
             ResultSet rs=ps.executeQuery();
-            ps.setString(1,student);
+            int count=0;
             if(rs.next()){
                 System.out.println(
                     "Press 1:To Update First Name\nPress 2: To Update Last Name\nPress 3:To Update Email\nPress 4:To Update Phone Number\nPress 5:To Update Course\nPress 6:To Update Batch\nPress 7:To Update Gender");
@@ -31,6 +31,7 @@ class updateDetail {
             else{
                 System.out.println("No Student is Found with this student ID in the Table");
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
