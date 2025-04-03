598648203f1f877e308fd862e04e15c8d6e14c88
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-03 11:08:05 +0530

    CURD

--- CURD_Application/src/UpdateDetail.java
+++ CURD_Application/src/UpdateDetail.java
@@ -8,7 +8,7 @@ class updateDetail{
         Scanner sc=new Scanner(System.in);
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:/3306/Libary_Mangement", "root", "@Radhakrishna297");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement", "root", "@Radhakrishna297");
             System.out.println("Code is Running");
         }
         catch(Exception e){
