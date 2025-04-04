15d48ea134d0d4417ffc73c57dda61649f14ed65
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-04 11:00:48 +0530

    CURD

---
+++ CURD_Application/src/Read.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+class fetchStatement{
+    public void readValue(){
+        try{
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    } 
+}
+public class Read{
+
+}
