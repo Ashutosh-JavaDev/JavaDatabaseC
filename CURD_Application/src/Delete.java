e6f683cc3bae6bc575198b874ca80da6e3ca5d10
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-04 12:03:02 +0530

    CURD

--- CURD_Application/src/Delete.java
+++ CURD_Application/src/Delete.java
@@ -7,6 +7,7 @@ class Deletion {
     void delete() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement","root","@Radhakrishna297");
 
         } catch (Exception e) {
             e.printStackTrace();
