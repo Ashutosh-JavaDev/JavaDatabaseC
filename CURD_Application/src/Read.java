c705561d25b3f104d4b4763335e51f9126d51912
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-04 11:07:40 +0530

    CURD

--- CURD_Application/src/Read.java
+++ CURD_Application/src/Read.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 class fetchStatement{
     public void readValue(){
         try{
-            Class .forName("com.cj.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangemant", "root", "@Radhakrishna");
+            Class .forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangement", "root", "@Radhakrishna");
             System.out.println("Program Run Successfully");
         }catch(Exception e){
             e.printStackTrace();
