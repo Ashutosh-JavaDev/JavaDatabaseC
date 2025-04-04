4c716481c7e9d29482da032e9118c30055e8992a
Ashutosh Ayush <jbnashu297@gmail.com>
2025-04-04 11:03:13 +0530

    CURD

--- CURD_Application/src/Read.java
+++ CURD_Application/src/Read.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 class fetchStatement{
     public void readValue(){
         try{
-
+            Class .forName("com.cj.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Libary_Mangemant", "root", "@Radhakrishna");
+            System.out.println("Program Run Successfully");
         }catch(Exception e){
             e.printStackTrace();
         }
